d17e269b124349849dca81ea0b4a8b3a4bd8fb51
RahulGandhi14 <gandhirahul274@gmail.com>
2020-09-22 13:49:59 +0530

    COMMIT BEFORE DEPLOYMENT

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -13,7 +14,7 @@ const postRoutes = require("./routes/post");
 
 //DB Connection
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -35,6 +36,14 @@ app.use("/api", postRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Starting a server
 app.listen(port, () => console.log(`app is running at ${port}`));
